c5b12277389c7f597c81f53375bc9b5d3f0b2a7f
lsc4473 <lsc4473@rit.edu>
2023-02-21 16:15:39 -0500

    formatting

--- Core/Src/timer.c
+++ Core/Src/timer.c
@@ -7,8 +7,7 @@ void TIM_Init(void) {
 	uint32_t tmpreg;
 	(void) tmpreg;
 
-	/* TIMER CONFIGURATION */
-
+	/* Timer Enable and Prescale */
 	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
 	tmpreg = RCC->APB1ENR1 & RCC_APB1ENR1_TIM2EN;
 	TIM2->PSC |= 15; 	// PSC set to 16 to divide frequency down to a period of 1us
@@ -19,20 +18,16 @@ void TIM_Init(void) {
 	TIM2->CCMR1 |= TIM_CCMR1_CC1S_0;
 	TIM2->CCMR1 &= ~(TIM_CCMR1_IC1F);
 	TIM2->CCER |= TIM_CCER_CC1E;
+
 	/* Update on the rising edge */
 	TIM2->CCER &= ~TIM_CCER_CC1P;
 	TIM2->CCER &= ~TIM_CCER_CC1NP;
 
-//	/* Enable Timer */
-//	TIM2->CR1 |= TIM_CR1_CEN;
-
 	// Setup GPIO for Alternate Function
 	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
 	GPIOA->MODER |= GPIO_MODER_MODE0_1;
-
 	GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL0;
 	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL0_0;
-
 
 	return;
 }
